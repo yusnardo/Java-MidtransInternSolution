35dba6a3b13478d9952542b2368ff4f2f1cfc57f
yusnardo <yus21newbie@gmail.com>
2018-03-27 23:16:15 +0700

    Commit 2
    Mencari persamaan data yang terdapat pada suatu alamat. Saya cuman membandingkan data jalan, nomor rumah, rt,rw dan kota.
    Kenapa saya tetap membandingkan RT dan RW? Kenapa tidak RT saja atau RW saja ? Karena saya memiliki pengalaman buruk terhadap dua data tersebut saat memesan barang secara online :)) . Disini saya berasumsi kalau kota hanya terdapat jakarta dan surabaya

--- Problem1.java
+++ Problem1.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Midtrans;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  *
  * @author Yusnardo
  */
 public class Problem1 {
+    boolean jalan;
+    boolean nomor;
+    boolean rt;
+    boolean rw;
+    boolean kota;
     
+    public Problem1() {
+        this.jalan = false;
+        this.nomor=false;
+        this.rt = false;
+        this.rw=false;
+        this.kota = false;
+    }
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(reader);
+        String input =in.readLine();
+
+        Problem1 alamatPertama=new Problem1();
+        alamatPertama.fillAddress(input);
+
+        input=in.readLine();
+        Problem1 alamatKedua=new Problem1();
+        alamatKedua.fillAddress(input);
+
+        Problem1.compare(alamatPertama,alamatKedua);
+    }
+    public void fillAddress(String input){
+        if(input.contains("jln")||input.contains("Jalan")||input.contains("jl")||input.contains("Jl")||input.contains("JL")||input.contains("Jln")){
+            this.jalan=true;
+        }
+        if(input.contains("NO.")||input.contains("no")||input.contains("nomor")||input.contains("Nomor")){
+            this.nomor=true;
+        }
+        if(input.contains("Rt.")||input.contains("rt")||input.contains("RT.")){
+            this.rt=true;
+        }
+        if(input.contains("Rw")||input.contains("rw")||input.contains("/")||input.contains("RW")){
+            this.rw=true;
+        }
+        //Asumsi kota hanya Surabaya dan Jakarta
+        if(input.contains("SBY")||input.contains("Jak")){
+            this.kota=true;
+        }
+    }
+    public static void compare(Problem1 pertama, Problem1 kedua){
+        if(pertama.jalan==true&&kedua.jalan==true){
+            System.out.print("Kedua alamat memiliki data nama jalan");
+        }
+        if(pertama.nomor==true&&kedua.nomor==true){
+            System.out.print("Kedua alamat memiliki data nomor rumah");
+        }
+        if(pertama.rt==true&&kedua.rt==true){
+            System.out.print("Kedua alamat memiliki data RT");
+        }
+        if(pertama.rw==true&&kedua.rw==true){
+            System.out.print("Kedua alamat memiliki data RW");
+        }
+        if(pertama.kota==true&&kedua.kota==true){
+            System.out.print("Kedua alamat memiliki data kota");
+        }  
+    }
 }
+
