7532c7a56b8fd7b5df8c897c3dd540f5a49ebbfd
yusnardo <yus21newbie@gmail.com>
2018-03-27 20:51:10 +0700

    Commit 2

--- Problem5.java
+++ Problem5.java
@@ -1,3 +1,5 @@
+package Midtrans;
+import java.util.Scanner;
 import java.time.LocalTime;
 import java.util.ArrayList;
 /**
@@ -6,6 +8,8 @@ import java.util.ArrayList;
  */
 public class Problem5 {
     public static void main (String[]args){
+        int[]manyBuses = new int[12];
+        int max=0,x=0;
         ArrayList<Bus> c = new ArrayList<>();
         ArrayList<Bus> d = new ArrayList<>();
         Bus bus1 = new Bus(LocalTime.of(10,0), LocalTime.of(10,5));
@@ -38,14 +42,21 @@ public class Problem5 {
                 }
             }
             System.out.println(now + " : " + c.size() + " Bus ");
+            manyBuses[x]=c.size(); 
+            x++;
         }
+        for(int i =0;i<manyBuses.length;i++){
+            if(manyBuses[i]>max)
+                max=manyBuses[i];
+        }
+        System.out.println("Maks number if bus inside the station at one point of time is "+max);
     }
 }
 class Bus{
     private LocalTime a;
     private LocalTime b;
     
-    public Bus(LocalTime a, LocalTime d) {
+    public Bus(LocalTime a, LocalTime b) {
         this.a = a;
         this.b = b;
     }
@@ -58,7 +69,7 @@ class Bus{
     public LocalTime getB() {
         return b;
     }
-    public void setD(LocalTime b) {
+    public void setB(LocalTime b) {
         this.b = b;
     }
 }
